7e083dd3d8a48fb75f71e6db35cd424951874de3
peytonjunjulas <97271349+peytonjunjulas@users.noreply.github.com>
2022-04-03 11:46:21 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+function clearHamock(){
+  $('input:radio[name=hamockType]').attr('checked', false);
+}
+
+function clearKayak(){
+  $('input:radio[name=kayakType]').attr('checked', false);
+}
+
+function clearCanoe(){
+  $('input:radio[name=canoeType]').attr('checked', false);
+}
